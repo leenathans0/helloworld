d8af4f3948dbe9a440af8bf8e9be245e44c672e8
Nathan Lee <leenathaniels@gmail.com>
2024-06-18 10:45:13 -0700

    My first commit

---
+++ helloworld.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// single line comment
+
+/*
+multi
+line
+comments
+*/
+
+int main(){
+    cout<<"hello world"<<endl;
+    cout<<"hello world\n";
+    //std::cout<<"hello"<<std::endl; //if we do not use namespace std above
+    return 0;
+}
